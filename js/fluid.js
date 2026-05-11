3f86c50888cf3a4f72d3fe0ab9b029bcaf0e5563
IsmaelRivDev <146744081+IsmaelRivela@users.noreply.github.com>
2026-05-11 20:43:22 +0200

    refactor: ismael->creative, work/cases->creative/cases, remove work/index.html

--- js/fluid.js
+++ js/fluid.js
@@ -378,7 +378,7 @@ export function initFluidDivider() {
     if (e.clientX < centerX) {
       window.location.href = '/vamps/'
     } else {
-      window.location.href = '/ismael/'
+      window.location.href = '/creative/'
     }
   })
 
